be9a4b911940b4a180f8b075f24e51ae09c65519
Quetzy Garcia <quetzyg@stowify.pt>
2023-03-24 21:23:37 +0000

    feat(Request): add MB Way request implementation

---
+++ src/Request/MBWayRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omnipay\IfThenPay\Request;
+
+use Omnipay\Common\Exception\InvalidRequestException;
+use Omnipay\Common\Message\ResponseInterface;
+use Omnipay\IfThenPay\Response\MBWayResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class MBWayRequest extends AbstractRequest
+{
+    private const CHANNEL    = '03';
+    private const BASE_URL   = 'https://mbway.ifthenpay.com';
+    private const MEDIA_TYPE = 'application/x-www-form-urlencoded';
+
+    /**
+     * @return array
+     * @throws InvalidRequestException
+     */
+    public function getData(): array
+    {
+        $this->validate(
+            'key',
+            'reference',
+            'amount',
+            'description',
+            'clientPhone',
+        );
+
+        return [
+            'canal'      => self::CHANNEL,
+            'MbWayKey'   => $this->getKey(),
+            'referencia' => $this->getReference(),
+            'valor'      => $this->getAmount(),
+            'descricao'  => $this->getDescription(),
+            'nrtlm'      => $this->getClientPhone(),
+            'email'      => $this->getClientEmail()
+        ];
+    }
+
+    /**
+     * @param  mixed  $data
+     * @return ResponseInterface
+     */
+    public function sendData($data): MBWayResponse
+    {
+        $headers = [
+            'Content-Type' => self::MEDIA_TYPE,
+        ];
+
+        $response = $this->httpClient->request(
+            Request::METHOD_POST,
+            $this->buildURL(self::BASE_URL, 'ifthenpaymbw.asmx/SetPedidoJSON'),
+            $headers,
+            http_build_query($data),
+        );
+
+        return new MBWayResponse($this, json_decode($response->getBody()->getContents(), true));
+    }
+}
