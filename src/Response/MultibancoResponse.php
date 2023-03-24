5079315f78c0facf3d6585f244d18391fecd7d59
Quetzy Garcia <quetzyg@stowify.pt>
2023-03-24 23:53:05 +0000

    feat(Response): add initial Multibanco response implementation

---
+++ src/Response/MultibancoResponse.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omnipay\IfThenPay\Response;
+
+use DateTimeImmutable;
+use Omnipay\Common\Message\AbstractResponse;
+
+class MultibancoResponse extends AbstractResponse
+{
+    private const CODE_SUCCESS = '0';
+
+    /**
+     * The Gateway's transaction reference.
+     *
+     * @return ?string
+     */
+    public function getTransactionReference(): ?string
+    {
+        if (! empty($this->data['RequestId'])) {
+            return $this->data['RequestId'];
+        }
+
+        return null;
+    }
+
+    /**
+     * The Merchant's transaction reference.
+     *
+     * @return ?string
+     */
+    public function getTransactionId(): ?string
+    {
+        if (! empty($this->data['OrderId'])) {
+            return $this->data['OrderId'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Payment gateway response code.
+     *
+     * @return ?string
+     */
+    public function getCode(): ?string
+    {
+        return $this->data['Status'];
+    }
+
+    /**
+     * Payment gateway response message.
+     *
+     * @return ?string
+     */
+    public function getMessage(): ?string
+    {
+        return $this->data['Message'];
+    }
+
+    /**
+     * Is the response successful?
+     *
+     * @return boolean
+     */
+    public function isSuccessful(): bool
+    {
+        return $this->getCode() === self::CODE_SUCCESS;
+    }
+
+    /**
+     * Multibanco payment entity.
+     *
+     * @return ?string
+     */
+    public function getEntity(): ?string
+    {
+        return $this->data['Entity'];
+    }
+
+    /**
+     * Multibanco payment reference.
+     *
+     * @return ?string
+     */
+    public function getReference(): ?string
+    {
+        return $this->data['Reference'];
+    }
+
+    /**
+     * Multibanco payment expiry.
+     *
+     * @return ?DateTimeImmutable
+     * @throws \Exception
+     */
+    public function getExpiry(): ?DateTimeImmutable
+    {
+        return new DateTimeImmutable(sprintf('%s 23:59:59', $this->data['ExpiryDate']));
+    }
+}
