c8474b51e6e7c13fd80825322735c5884e209a55
Quetzy Garcia <quetzyg@stowify.pt>
2023-03-24 23:22:42 +0000

    feat(Gateway): initial implementation

---
+++ src/Gateway.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omnipay\IfThenPay;
+
+use Omnipay\Common\AbstractGateway;
+use Omnipay\Common\Http\ClientInterface;
+use Omnipay\Common\Message\RequestInterface;
+use Omnipay\IfThenPay\Enum\PayMethod;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+class Gateway extends AbstractGateway
+{
+    private PayMethod $payMethod;
+
+    /**
+     * @param  PayMethod  $payMethod
+     * @param  ?ClientInterface  $httpClient
+     * @param  ?SymfonyRequest  $httpRequest
+     */
+    public function __construct(PayMethod $payMethod, ClientInterface $httpClient = null, SymfonyRequest $httpRequest = null)
+    {
+        $this->payMethod = $payMethod;
+
+        parent::__construct($httpClient, $httpRequest);
+    }
+
+    /**
+     * The gateway display name.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'IfThenPay';
+    }
+
+    /**
+     * Get the default parameters of the gateway channel.
+     *
+     * @return array
+     */
+    public function getDefaultParameters(): array
+    {
+        return $this->payMethod->defaultParameters();
+    }
+
+    public function purchase(array $options = []): RequestInterface
+    {
+        return $this->createRequest($this->payMethod->requestClass(), $options);
+    }
+}
