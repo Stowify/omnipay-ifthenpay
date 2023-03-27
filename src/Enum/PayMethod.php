e8f46a4e8f4f9f1f723a051028933d3d6f561d90
Quetzy Garcia <quetzyg@stowify.pt>
2023-03-27 17:43:07 +0100

    feat(Enums): add Card to PayMethod

--- src/Enum/PayMethod.php
+++ src/Enum/PayMethod.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace Omnipay\IfThenPay\Enum;
 
+use Omnipay\IfThenPay\Request\CardRequest;
 use Omnipay\IfThenPay\Request\MBWayRequest;
 use Omnipay\IfThenPay\Request\MultibancoRequest;
 
 enum PayMethod: string
 {
+    case Card       = 'card';
     case MBWay      = 'mbway';
     case Multibanco = 'multibanco';
 
     public function defaultParameters(): array
     {
         return match ($this) {
+            self::Card => [
+                'reference'  => '',
+                'amount'     => '',
+                'successUrl' => '',
+                'errorUrl'   => '',
+                'cancelUrl'  => '',
+                'language'   => 'en',
+            ],
             self::MBWay => [
                 'key'         => '', // Required
                 'reference'   => '', // Required
@@ -43,6 +53,7 @@ enum PayMethod: string
     public function requestClass(): string
     {
         return match ($this) {
+            self::Card       => CardRequest::class,
             self::MBWay      => MBWayRequest::class,
             self::Multibanco => MultibancoRequest::class,
         };
